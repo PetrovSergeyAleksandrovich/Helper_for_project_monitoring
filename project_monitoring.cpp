97fdb8f505b8597bea8c9317c8e12551f96fcca9
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-05 22:44:40 +0300

    amended functions. eng cout

--- project_monitoring.cpp
+++ project_monitoring.cpp
@@ -3,28 +3,18 @@
 #include <string>
 #include <Windows.h>
 
-int main()
+void print_projects_list()
 {
-    //Init info
-    std::cout << "=====PROJECTS_LIST=====" << std::endl;
-    setlocale(LC_ALL, "Rus");
-
-    //Variables
     std::string file_location = "projects.txt";
-    std::string command = "none";
     char buffer[2];
-    std::string text_to_add;
-
-    //Read file contains
     std::ifstream file_read;
     file_read.open(file_location, std::ios::binary);
     while (!file_read.is_open())
     {
-        std::cout << "\nВведите новый путь к файлу/Type new path to file:";
+        std::cout << "\nType path to file:";
         std::getline(std::cin, file_location);
         file_read.open(file_location, std::ios::binary);
     }
-
     while (!file_read.eof())
     {
         file_read.read(buffer, sizeof(buffer) - 1);
@@ -32,43 +22,55 @@ int main()
         std::cout << buffer;
     }
     file_read.close();
+}
+
+int main()
+{
+    //Init info
+    std::cout << "=====PROJECTS_LIST=====" << std::endl << std::endl;
+    setlocale(LC_ALL, "Rus");
+    //Variables
+    std::string command = "none";
+    std::string text_to_add;
+
+    //Read file contains
+    print_projects_list();
 
     //User input new project info
-    std::cout << std::endl << "\nВведите текст / Input text: ";
+    std::cout << std::endl << "\nType project number and discription: ";
     SetConsoleCP(1251);
     std::getline(std::cin, text_to_add);
     SetConsoleCP(866);
-    std::cout << "\nТекст для добавления / Your text to add: ";
+    std::cout << "\nYour text to add: ";
     //SetConsoleOutputCP(1251);
     std::cout << text_to_add << std::endl;
     SetConsoleOutputCP(866);
 
     //Check and confirm input from user
-    std::cout << "\nНажмите 1 подтвердить. 0 для повторного ввода\nPress 1 to confirm. Press 0 to repeat input: ";
+    std::cout << "\nPress 1 to confirm. 0 to type again: ";
     std::getline(std::cin, command);
     while (command != "1")
     {
-        // SetConsoleOutputCP(1251);
-
         //User input new project info
         text_to_add.clear();
         if (text_to_add.empty())
         {
-            std::cout << "\nВведите текст снова / Input text again: ";
-            // SetConsoleCP(1251);
+            std::cout << "\nType project number and discription: ";
+            SetConsoleCP(1251);
             std::getline(std::cin, text_to_add);
-            std::cout << "\nТекст для добавления / Text to add: " << text_to_add << std::endl;
+            SetConsoleCP(866);
+            std::cout << "\nYour text to add: " << text_to_add << std::endl;
+            SetConsoleOutputCP(866);
         }
-
-        std::cout << "\nНажмите 1 подтвердить. 0 для повторного ввода\nPress 1 to confirm. Press 0 to repeat input: ";
+        std::cout << "\nPress 1 to confirm. 0 to type again: ";
         std::getline(std::cin, command);
         if (command == "1") break;
-
     }
 
     //Add new text to file
     if (!text_to_add.empty())
     {
+        std::string file_location = "projects.txt";
         std::ofstream file_write(file_location, std::ios::app);
         SetConsoleOutputCP(1251);
         file_write << std::endl << text_to_add;
@@ -76,25 +78,17 @@ int main()
         file_write.close();
 
         //Read file contains
-        std::cout << std::endl << std::endl << "=====PROJECTS_LIST_NEW=====" << std::endl;
-        file_read.open(file_location, std::ios::binary);
-        while (!file_read.eof())
-        {
-            file_read.read(buffer, sizeof(buffer) - 1);
-            buffer[file_read.gcount()] = 0;
-            //SetConsoleOutputCP(1251);
-            std::cout << buffer;
-        }
-        file_read.close();
-        std::cout << std::endl << std::endl << std::endl;
-        //SetConsoleOutputCP(1251);
-        std::cout << "Информация успешно добавлена в файл\nInformation successfully added to file" << std::endl << std::endl;
+        print_projects_list();
+
+        std::cout << std::endl << std::endl;
+        std::cout << "Information successfully added to file" << std::endl;
     }
     else
     {
-        std::cout << "\n\n===Пустая запись/Empty record===\n\n";
+        std::cout << "\n\n===Empty record===\n\n";
     }
 
+    std::cout << "\n\n===Made in Russia / Design Systems LLC / www.dsys.ru===\n\n";
     std::cin.get();
     return 0;
 }
