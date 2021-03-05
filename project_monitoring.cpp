ee9b99595fcba373c730427e54bb91985062868c
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-03-05 17:19:14 +0300

    Ru and Eng languages supported to input

--- project_monitoring.cpp
+++ project_monitoring.cpp
@@ -10,19 +10,19 @@ int main()
     setlocale(LC_ALL, "Rus");
 
     //Variables
-    std::string file_location = R"(C:\Users\Poizone\Projects_Monitoring\projects.txt)";
+    std::string file_location = "projects.txt";
     std::string command = "none";
     char buffer[2];
     std::string text_to_add;
 
     //Read file contains
-    std::ifstream file_read(file_location, std::ios::binary);
-
+    std::ifstream file_read;
+    file_read.open(file_location, std::ios::binary);
     while (!file_read.is_open())
     {
-        std::cout << "\nType new path to file:";
-        std::cin >> file_location;
-        std::ifstream file_read(file_location, std::ios::binary);
+        std::cout << "\nВведите новый путь к файлу/Type new path to file:";
+        std::getline(std::cin, file_location);
+        file_read.open(file_location, std::ios::binary);
     }
 
     while (!file_read.eof())
@@ -33,32 +33,34 @@ int main()
     }
     file_read.close();
 
-
-
     //User input new project info
-    std::cout << std::endl << "\nInput text: ";
+    std::cout << std::endl << "\nВведите текст / Input text: ";
     SetConsoleCP(1251);
     std::getline(std::cin, text_to_add);
-    SetConsoleOutputCP(1251);
-    std::cout << "\nYour text to add: " << text_to_add << std::endl;
+    SetConsoleCP(866);
+    std::cout << "\nТекст для добавления / Your text to add: ";
+    //SetConsoleOutputCP(1251);
+    std::cout << text_to_add << std::endl;
+    SetConsoleOutputCP(866);
 
     //Check and confirm input from user
-    std::cout << "\nnPress 1 to confirm. Press 0 to repeat input: ";
+    std::cout << "\nНажмите 1 подтвердить. 0 для повторного ввода\nPress 1 to confirm. Press 0 to repeat input: ";
     std::getline(std::cin, command);
     while (command != "1")
     {
-        SetConsoleOutputCP(1251);
+        // SetConsoleOutputCP(1251);
+
         //User input new project info
         text_to_add.clear();
         if (text_to_add.empty())
         {
-            std::cout << "\nInput text again: ";
-            SetConsoleCP(1251);
+            std::cout << "\nВведите текст снова / Input text again: ";
+            // SetConsoleCP(1251);
             std::getline(std::cin, text_to_add);
-            std::cout << "\nText to add: " << text_to_add << std::endl;
+            std::cout << "\nТекст для добавления / Text to add: " << text_to_add << std::endl;
         }
 
-        std::cout << "\nPress 1 to confirm. Press 0 to repeat input: ";
+        std::cout << "\nНажмите 1 подтвердить. 0 для повторного ввода\nPress 1 to confirm. Press 0 to repeat input: ";
         std::getline(std::cin, command);
         if (command == "1") break;
 
@@ -68,7 +70,9 @@ int main()
     if (!text_to_add.empty())
     {
         std::ofstream file_write(file_location, std::ios::app);
+        SetConsoleOutputCP(1251);
         file_write << std::endl << text_to_add;
+        SetConsoleOutputCP(866);
         file_write.close();
 
         //Read file contains
@@ -78,16 +82,17 @@ int main()
         {
             file_read.read(buffer, sizeof(buffer) - 1);
             buffer[file_read.gcount()] = 0;
+            //SetConsoleOutputCP(1251);
             std::cout << buffer;
         }
         file_read.close();
         std::cout << std::endl << std::endl << std::endl;
-        SetConsoleOutputCP(1251);
-        std::cout << "Information successfully added to file" << std::endl << std::endl;
+        //SetConsoleOutputCP(1251);
+        std::cout << "Информация успешно добавлена в файл\nInformation successfully added to file" << std::endl << std::endl;
     }
     else
     {
-        std::cout << "\n\n===Empty record===\n\n";
+        std::cout << "\n\n===Пустая запись/Empty record===\n\n";
     }
 
     std::cin.get();
